5563d793e4ebc4fb88e019b55abceea4777d8d7d
mandy <mandyhale10@gmail.com>
2022-12-04 20:26:17 +1300

    create empty contact form files and a post route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +21,18 @@ Route::get('/', function () {
 
 Route::get('/counter', function () {
   return view('counter');
+});
+
+Route::post('/contact', function(Request $request){
+  $contact = $request->validate([
+    'name' => 'required',
+    'email' => 'required|email',
+    'phone' => 'required',
+    'message' => 'required'
+  ]);
+
+  Mail::to('mandyhale10@gmail.com')->send(new ContactFormMailable($contact));
+
+  return back()->with('success_message', 'We received your message successffully and will get back to you shortly. Thank you!')
+
 });
