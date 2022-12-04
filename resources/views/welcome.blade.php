9ad3714ed704d556fbbc664a70b9bc244d8a03cd
mandy <mandyhale10@gmail.com>
2022-12-04 20:18:56 +1300

    styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,15 @@
 
     </head>
     <body>
+      <header>
+        <h1>Welcome to my livewire projects</h1>
+        {{-- TODO write the nav class --}}
+        <nav class="topnav">
+          <a class="active" href="#home">Home</a>
+          <a href="#counter">Counter</a>
+          <a href="#contact-form">Contact form</a>
+        </nav>
+      </header>
       
       <livewire:counter />
       
